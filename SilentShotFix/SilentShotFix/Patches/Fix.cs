f65f01d2a4a1ce93a09ea703ebceff1dbd380868
Christopher Teo <40913834+randomuserhi@users.noreply.github.com>
2024-08-22 06:48:45 +0100

    Initial

---
+++ SilentShotFix/SilentShotFix/Patches/Fix.cs
@@ -0,0 +1,25 @@
+﻿using Gear;
+using HarmonyLib;
+using SNetwork;
+
+namespace BioScannerFix {
+    [HarmonyPatch]
+    internal static class Fix {
+        private static ItemEquippable? oldItem;
+
+        [HarmonyPatch(typeof(PlayerInventorySynced), nameof(PlayerInventorySynced.GetSync))]
+        [HarmonyPrefix]
+        private static void Prefix_ShotSync(PlayerInventorySynced __instance) {
+            if (!SNet.IsMaster || __instance.Owner.Owner.IsBot) return;
+
+            if (__instance.m_queuedEquipItem != null) {
+                ItemEquippable item = __instance.m_queuedEquipItem;
+
+                BulletWeaponSynced? bulletWeapon = item.TryCast<BulletWeaponSynced>();
+                if (bulletWeapon != null) {
+                    bulletWeapon.Fire();
+                }
+            }
+        }
+    }
+}
